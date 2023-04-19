628ad2022657e2064fe42708c2ce312fdf26f978
tom <tommaso.puzzo@stud.unifi.it>
2023-04-19 10:57:27 +0200

    huuuuuuhhhuhuhh

--- scripts/jscripts.js
+++
@@ -1,7 +0,0 @@
-$(window).scroll(function() {
-    if ($(window).scrollTop() > 100) {
-        $('.menu').addClass('sticky-menu');
-    } else {
-        $('.menu').removeClass('sticky-menu');
-    }
-});
