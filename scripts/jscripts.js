0982a3b376f1b70a89de66cb90d80ef50495dc64
tom <tommaso.puzzo@stud.unifi.it>
2023-04-19 15:46:54 +0200

    TBD tent menu of sticky menu

--- scripts/jscripts.js
+++ scripts/jscripts.js
@@ -6,44 +6,6 @@ function mostraMenu() {
 }
 
 
-
-var centralMenu = document.getElementById("central-menu");
-var scrolledMenu = document.getElementById("scrolled-menu");
-
-window.onscroll = function() {
-    if (window.pageYOffset > 50) {
-        centralMenu.style.display = "none";
-        scrolledMenu.style.display = "block";
-    } else {
-        centralMenu.style.display = "block";
-        scrolledMenu.style.display = "none";
-    }
-}
-
-
-var dropdownMenu = document.getElementById("dropdown-menu");
-
-// Aggiungi l'evento di clic a ciascuna voce del menu central
-var centralMenuItems = document.querySelectorAll("#central-menu li a");
-centralMenuItems.forEach(function(item) {
-    item.addEventListener("click", function(event) {
-        event.preventDefault();
-        //codice per mostrare il menu a tendina
-        mostraMenu();
-    });
-});
-
-// Aggiungi l'evento di clic a ciascuna voce del menu scrolled
-var scrolledMenuItems = document.querySelectorAll("#scrolled-menu li a");
-scrolledMenuItems.forEach(function(item) {
-    item.addEventListener("click", function(event) {
-        event.preventDefault();
-        // codice per mostrare il menu a tendina
-        mostraMenu();
-    });
-});
-
-
 document.getElementById('menu-1-toggler').addEventListener('click', function(e) {
     e.preventDefault();
     console.log('clic');
